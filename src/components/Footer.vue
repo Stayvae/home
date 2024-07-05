4dbf526b7382102e7c729e48296bb204f8bca2d1
Stay <130443969+Stayvae@users.noreply.github.com>
2024-07-05 18:41:20 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,13 @@
           {{ fullYear }}
           <a :href="siteUrl">{{ siteAnthor }}</a>
         </span>
-        
+        <!-- 以下信息请不要修改哦 -->
+        <span class="hidden">
+          
+          <a :href="config.github" target="_blank">
+            {{ config.author }}
+          </a>
+        </span>
         <!-- 站点备案 -->
         <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
           &amp;
